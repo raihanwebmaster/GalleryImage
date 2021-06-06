722223d296cc2d0d8eb90b3dc81f0eeec25dcb70
raihanwebmaster <raihanemon2015@gmail.com>
2021-06-06 21:34:59 +0200

    filter-working new function

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GalleryReact from "./component/TabMenu/GalleryReact";
+
+const App = () => {
+  return (
+    <>
+     <GalleryReact />
+    </>
+  )
+}
+
+export default App
